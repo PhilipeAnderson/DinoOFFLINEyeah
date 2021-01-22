13c14db354e29ee6885cc88531f2de4b6ca01f22
PhilipeAnderson <philipeacampos@hotmail.com>
2021-01-22 15:23:54 -0300

    Criado as primeiras movimentações dos personagens

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 //Acesse o site keycode.info e descubra o valor de keyCode de cada tecla
 function handlekeyUp(event) {
     if (event.keyCode === 32) {
-        jump();
+        if(!isJumping) {
+            jump();
+        }   
     }
 };
 
 function jump() {
     let position = 0;
 
+    isJumping = true;
+
     let upInterval = setInterval(() => {
-        if (position >= 150) {
+        if (position >= 220) {
             clearInterval(upInterval);
 // Dino desce
             let downInterval = setInterval(() =>{
                 if(position  <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     position -= 15;
                     dino.style.bottom = position + 'px';
@@ -31,4 +38,19 @@ function jump() {
     }, 15);
 }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+     
+    let leftInterval = setInterval(() => {
+        cactusPosition -= 10;
+        cactus.style.left = cactusPosition + 'px';
+    }, 20)
+}
+
+createCactus();
 document.addEventListener('keyup', handlekeyUp);
